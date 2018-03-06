eb2f3065ab5c918cfc34cc18412978e15821dc57
李鑫 <lixin544@163.com>
2018-03-06 21:43:09 +0800

    init

---
+++ build/build.js
@@ -0,0 +1,30 @@
+const webpack = require('webpack')
+const path = require('path')
+const rm = require('rimraf')
+const ora = require('ora')
+const chalk = require('chalk')
+const webpackConfig = require('./webpack.config')
+
+const spinner = ora('building for production...')
+spinner.start()
+rm(path.resolve(__dirname, '../dist'), err => {
+    if (err) throw err
+    webpack(webpackConfig, function (err, stats) {
+        spinner.stop()
+        if (err) throw err
+        process.stdout.write(stats.toString({
+            colors: true,
+            modules: false,
+            children: false,
+            chunks: false,
+            chunkModules: false
+        }) + '\n\n')
+
+        if (stats.hasErrors()) {
+            console.log(chalk.red('  Build failed with errors.\n'))
+            process.exit(1)
+        }
+
+        console.log(chalk.cyan('  Build complete.\n'))
+    })
+})
